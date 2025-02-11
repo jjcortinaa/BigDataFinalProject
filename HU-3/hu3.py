a4fc9a3543df1bc972ae36d4497bbb7c7f747231
XabierAlbizu <202207372@alu.comillas.edu>
2025-02-11 18:34:50 +0100

    HU-3

--- HU-3/hu3.py
+++ HU-3/hu3.py
@@ -7,9 +7,6 @@ CRAWLER_NAME = "trade_data_crawler"
 S3_TARGET_PATH = "s3://h2datacortialbizuclaudia/data/"  # Cambia con tu bucket
 IAM_ROLE = "arn:aws:iam::288761731908:role/AWSGlueServiceRole"  # Cambia con tu rol de Glue
 
-aws_access_key_id="ASIAUGO4KLNCM4EEQD4P"
-aws_secret_access_key="6QXfKpMt6HOt1ebn9oCrihX96veCUiOA7+Foz1pH"
-aws_session_token="IQoJb3JpZ2luX2VjEKn//////////wEaCmV1LXNvdXRoLTIiRzBFAiBrdl8x53sVT8z8UBdEhDuUuIJOY0JzB/0YAEpND875TQIhAPYSg2Z7W2hlyeqihbNcSDz030YHFRLD1fo2cKT6muWmKukCCEsQABoMMjg4NzYxNzMxOTA4Igw5VNc0vpEIVDncBlAqxgKv8d6RSIkSMm3LBl9nnAqnC2ERVzB1y3rfVeHIMFY9tbUr03gIQHJ6VjE1BV0fTFJ/pchaNUHMdWk+qvgFDJGg1BbIcNOWBfpud0mNwdX0vDtvFt5ZDe0fV2OFnTkKh9gxUIJbC9WBsjpw8Kf3BIzUkonGimHvjIXVrW0+5DXvfuDgaPj1SASAjw0bUV5uMuIHFzm+NwS2qIm83/7DFx1YgH4EXXZM6YzOnveDr8ym6edbARybB+rA9qK0of7Em6q7Gz7a1onrKeVk5lsHePOAlDjjZ6Ky7HO53UC1kRBsw6aL/KnCDBxG0I8hHTZFz1oAqN6VL5mj7kNPiZYEbLvSlnvC/+vN+UGzL1ajtI9rVflgTew1GA4hfmj2xPbTKNPnL4ASc6eMJ5tbV+HLe1aI4uD/LTB5Cc1sf1TXsrR7KhuYABmr3jDK1469BjqnAXu6tKCT7w0XS8SoqRxQugjui/V6+/FQYoVXh/HLIWe+Wq7dY/Rj8XBtBEEV79lbl44khtPYI8LYmpHbt3KUDVHVnLEDcdK5B2Vdk8GFvtPSwtBSyFuOLqQi3/qgpnJ83UpGd6mwCotonzFeoezUK3EOIOP6B7UnHwxHqJrfoQBKS+9Z+hLQ6nEBjNpWyI7XI524993eKnsyBFxKEv7s9xd5lYrY9noD"
 # Inicializar el cliente de AWS Glue
 glue_client = boto3.client('glue', region_name=AWS_REGION,
                            aws_access_key_id = aws_access_key_id,
